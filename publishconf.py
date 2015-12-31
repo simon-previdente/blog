69de601a7fabb77caf57bfc87933326c67b9454f
Simon Prévidente <simon-previdente@users.noreply.github.com>
2015-12-31 13:26:19 +0100

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://simon-previdente.github.io/blog'
+SITEURL = '//simon-previdente.github.io/blog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
